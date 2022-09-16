f4c4d3232acc55901870bb6a7141bf52c75a678b
Thomas Bock <thomas.bock@ptb.de>
2022-09-16 16:05:00 +0200

    init co

--- src/vl_mpd_graph/core.clj
+++ src/vl_mpd_graph/core.clj
@@ -1,42 +1,40 @@
-(ns graphclj.core
+(ns vl-mpd-graph.core
   (:require [libcdb.core :as db]
             [libcdb.configure :as cf]
             [tangle.core :as tangle]
             [clojure.string :as string]
             [clojure.java.io :as io]))
 
-(defn- flattenv [v] (into [] (flatten v)))
+(defn flattenv [v] (into [] (flatten v)))
+
+(defn post-edge [m]
+  (->> m
+       (filterv some?)
+       (mapv (fn [{:keys [from to]}] [from to]))))
 
 (defn walk [groups f]
-  (flattenv
-   (mapv (fn [{:keys [Definition] :as group} ndx]
-           (mapv (fn [p idx]
-                   (mapv (fn [task jdx]
-                           (f group task ndx idx jdx))
+  (mapv (fn [{:keys [Definition] :as group} ndx]
+          (mapv (fn [p idx]
+                  (mapv (fn [task jdx]
+                          (f group task ndx idx jdx))
                         p (range)))
-                 Definition (range)))
-         groups (range))))
+                Definition (range)))
+        groups (range)))
 
 (defn prefix [group] (if (:Condition group) "defins" "cont"))
 
 (defn id [group ndx idx jdx]
   (keyword (string/join "_" [(prefix group) ndx idx jdx]))) 
 
-
 (defn nodes [group {:keys [TaskName Replace Use]} ndx idx jdx]
   {:id (id group  ndx idx jdx)
    :color "blue"
    :label [:b TaskName]})
 
-(defn edges-map [group task ndx idx jdx]
+(defn edges [group task ndx idx jdx]
   (if (pos? idx)
     {:from (id group ndx (dec idx) jdx)
      :to (id group ndx idx jdx)}))
-
-(defn edges [m]
-  (->> m
-       (filterv some?)
-       (mapv (fn [{:keys [from to]}] [from to]))))
 
 (defn get-mpd [id]
   (->> {:prot "http",
@@ -50,19 +48,16 @@
 
 
 (comment
-  (def n (walk (-> (get-mpd "mpd-ce3-bake_out")
-                   :Mp
-                   :Container)
-               nodes))
+  (def mpd (get-mpd "mpd-se3-calib"))
   
-  (def e (edges (walk (-> (get-mpd "mpd-ce3-bake_out")
-                          :Mp
-                          :Container)
-                      edges-map)))
+  (def cont (-> mpd :Mp :Container))
+  
+  (def n (flattenv (walk cont nodes)))
+  
+  (def e (post-edge (flattenv (walk cont edges))))
   
   (def dot (tangle/graph->dot n e {:node {:shape :box}
-                            :node->id (fn [n] (if (keyword? n) (name n) (:id n)))
-                            :node->descriptor (fn [n] (when-not (keyword? n) n))
-                            }))
+                                   :node->id (fn [n] (if (keyword? n) (name n) (:id n)))
+                                   :node->descriptor (fn [n] (when-not (keyword? n) n))}))
 
   (io/copy (tangle/dot->image dot "png") (io/file "hello.png")))
